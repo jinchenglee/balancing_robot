af4c95b7a37860ad314fa9228abb334fcb211871
jinchenglee <jincheng.li@gmail.com>
2017-04-03 16:04:19 +0000

    WIP: adding imu initial measurements for GyroY static offset.

--- src/main.cpp
+++ src/main.cpp
@@ -14,53 +14,6 @@
  
 using namespace std;
 
-int imu_test() {
-  cout << "MPU6050 3-axis acceleromter example program\n";
-  I2Cdev::initialize();
-  MPU6050 accelgyro ;
-  int16_t ax, ay, az;
-  int16_t gx, gy, gz;
-  if ( accelgyro.testConnection() ) 
-    printf("MPU6050 connection test successful\n") ;
-  else {
-    fprintf( stderr, "MPU6050 connection test failed! something maybe wrong, continuing anyway though ...\n");
-    //return 1;
-  }
-  accelgyro.initialize();
-  // use the code below to change accel/gyro offset values
-  /*
-  printf("Updating internal sensor offsets...\n");
-  // -76	-2359	1688	0	0	0
-  printf("%i \t %i \t %i \t %i \t %i \t %i\n", 
-	 accelgyro.getXAccelOffset(),
-	 accelgyro.getYAccelOffset(),
-	 accelgyro.getZAccelOffset(),
-	 accelgyro.getXGyroOffset(),
-	 accelgyro.getYGyroOffset(),
-	 accelgyro.getZGyroOffset());
-  accelgyro.setXGyroOffset(220);
-  accelgyro.setYGyroOffset(76);
-  accelgyro.setZGyroOffset(-85);
-  printf("%i \t %i \t %i \t %i \t %i \t %i\n", 
-	 accelgyro.getXAccelOffset(),
-	 accelgyro.getYAccelOffset(),
-	 accelgyro.getZAccelOffset(),
-	 accelgyro.getXGyroOffset(),
-	 accelgyro.getYGyroOffset(),
-	 accelgyro.getZGyroOffset());
-  */
-  
-  printf("\n");
-  printf("  ax \t ay \t az \t gx \t gy \t gz:\n");
-  while (true) {
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    printf("  %d \t %d \t %d \t %d \t %d \t %d\r", ax, ay, az, gx, gy, gz);
-    fflush(stdout);
-    bcm2835_delay(100);
-  }
-  return 1; 
-}
- 
 int main(int argc, char **argv)
 {
     // GPIO pin assignment
@@ -87,19 +40,45 @@ int main(int argc, char **argv)
     // motors init
     motor_a.init(PWMA, AIN1, AIN2);
     motor_b.init(PWMB, BIN1, BIN2);
- 
+
+    // IMU init
+    int16_t ax, ay, az;
+    int16_t gx, gy, gz;
+    I2Cdev::initialize();
+    MPU6050 imu ;
+    if ( imu.testConnection() ) 
+      cout << "MPU6050 connection test successful\n";
+    else {
+      cerr << "MPU6050 connection test failed! Exit.\n";
+      return 1;
+    }
+    imu.initialize();
+
+    // Average out measurements of GyroY to get static offset at start position.
+    int16_t gyroY_static = 0;
+    int GYROY_INIT_MEASURE = 25;
+    for (int i=0;i<GYROY_INIT_MEASURE;i++) {
+        gyroY_static += imu.getRotationY();
+        bcm2835_delay(10);
+    }
+    gyroY_static = gyroY_static/GYROY_INIT_MEASURE;
+
+
+    imu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    printf("  %d \t %d \t %d \t %d \t %d \t %d\r", ax, ay, az, gx, gy, gz);
+    fflush(stdout);
+    bcm2835_delay(100);
+
     // main loop
     int i=0;
     for(i=0; i<3; i++)
     {
-	// Turn it on
         motor_a.forward();
         motor_b.forward();
  
 	// wait a bit
 	delay(500);
  
-	// turn it off
         motor_a.backward();
         motor_b.backward();
  
@@ -110,7 +89,5 @@ int main(int argc, char **argv)
     motor_a.stop();
     motor_b.stop();
  
-    imu_test();
-
     return 0;
 }
